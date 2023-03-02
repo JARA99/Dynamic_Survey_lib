8f07e6eb0f3557fcf3aab2f2d98462b3e345f893
Jorge Alejandro <jorgealejandro1999@gmail.com>
2023-03-02 00:08:05 -0600

    generate_dataset done

--- LinearClassifier/generate_dataset.py
+++ LinearClassifier/generate_dataset.py
@@ -1,3 +1,23 @@
 # File name: generate_dataset.py
 # Author: Jorge Alejandro Rodriguez Aldana
 # Date: 1mar2023
+
+# Import libraries
+# -----------------
+
+import numpy as np
+from .basic_linear_classifier import blc_predictor
+
+# Generate dataset from weight
+# ----------------------------
+
+def generate_dataset(w:np.ndarray,n:int = 100):
+    dataset = []
+    dimension = w.shape[0]
+
+    for data_n in range(n):
+        x = np.random.randn(dimension)
+        y, score = blc_predictor(w,x)
+
+        dataset.append((x,y))
+
