9f42ffdfd9f6cb3686eb35cffc94f19f8d827d8c
Jorge Alejandro <jorgealejandro1999@gmail.com>
2023-06-25 15:34:01 -0600

    adding probability and condition functions

--- pydyn_surv/classes/funcs.py
+++ pydyn_surv/classes/funcs.py
@@ -16,7 +16,7 @@ def TRAIN_FUNCTION(self,gradient_des:callable = blc.gradient_descent,eta:float =
     dataset = self.get_training_dataset()
     w = self.get_weight()
     trained_w = gradient_des(blc.squared_loss,blc.squared_loss_derivative,dataset,eta,iter_,verbose,w)
-    
+
     return trained_w
 
 def FUNC_LIKERT_ITEM_PROBABILITY(self,axis_move = 2):
@@ -33,35 +33,23 @@ def FUNC_LIKERT_ITEM_PROBABILITY(self,axis_move = 2):
         
         return prob
 
-def FUNC_APPLY_TO_ITEM_AND_SURVEY_HISTORY(self,func:callable,*args,**kargs):
+def FUNC_APPLY_TO_ITEM_AND_CATEGORY_HISTORY(self,func:callable,*args,**kargs):
 
 
-    categories = np.where(self.category_vector != 0)[0]
+    categories = np.where(self.category_vector != 0)[0] # [0] is necessary to get the array from the tuple.
     answer_history = self.get_answer_history()
     srv_cat_history = self.get_origin_survey().get_category_answer_history()
 
-    srv_func_list = []
+    cat_func_list = []
     for i in categories:
-        try:
-            func_to_cat = func(srv_cat_history[i],*args,**kargs)
-        except:
-            func_to_cat = np.nan
-        srv_func_list.append(func_to_cat)
-    
-    try:
-        item_func = func(answer_history,*args,**kargs)
-    except:
-        item_func = np.nan
+        func_to_cat = func(srv_cat_history[i],*args,**kargs)
+        cat_func_list.append(func_to_cat)
 
-    if np.isnan(srv_func_list).all():
-        srv_func = np.nan
-    else:
-        try:
-            srv_func = np.nanmean(srv_func_list)
-        except:
-            srv_func = np.nan
 
-    return item_func, srv_func
+    cat_func = np.nanmean(cat_func_list)
+    item_func = func(answer_history,*args,**kargs)
+
+    return item_func, cat_func
 
 
 def FUNC_LIKERT_ITEM_PROBABILITY_WITH_STATISTICS(self,axis_move = 2,not_repeated_since = 5,std_weight = SELF_STD_W,cat_std_weight = CAT_STD_W,launch_count_weight = SELF_COUNT_W,cat_launch_count_weight = CAT_COUNT_W):
@@ -76,20 +64,20 @@ def FUNC_LIKERT_ITEM_PROBABILITY_WITH_STATISTICS(self,axis_move = 2,not_repeated
         # print("Not repeated since ",launched_since," launches")
         return 0
 
-    item_std, surv_std = FUNC_APPLY_TO_ITEM_AND_SURVEY_HISTORY(self,np.std)
-    item_count, surv_count = FUNC_APPLY_TO_ITEM_AND_SURVEY_HISTORY(self,len)
+    item_std, cat_std = FUNC_APPLY_TO_ITEM_AND_CATEGORY_HISTORY(self,FUNC_STD)
+    item_count, cat_count = FUNC_APPLY_TO_ITEM_AND_CATEGORY_HISTORY(self,len)
 
     if srv_launch_count > 0:
         item_count_percent = item_count / srv_launch_count
-        surv_count_percent = surv_count / srv_launch_count
+        cat_count_percent = cat_count / srv_launch_count
     else:
         item_count_percent = 0
-        surv_count_percent = 0
+        cat_count_percent = 0
 
-    prob_elements = [axis_move,self.expertvalue,item_std*std_weight,surv_std*cat_std_weight,item_count_percent*launch_count_weight,surv_count_percent*cat_launch_count_weight]
+    prob_elements = [axis_move,self.expertvalue,item_std*std_weight,cat_std*cat_std_weight,item_count_percent*launch_count_weight,cat_count_percent*cat_launch_count_weight]
 
     if np.isnan(prob_elements).all():
-        srv_func = np.nan
+        prob = 0
     else:
         prob = np.nansum(prob_elements)
 
@@ -99,5 +87,33 @@ def FUNC_LIKERT_ITEM_PROBABILITY_WITH_STATISTICS(self,axis_move = 2,not_repeated
     return prob
 
 
+def FUNC_STD(l:list,return_when_empty = 0):
+    if len(l) == 0:
+        return return_when_empty
+    return np.std(l)
+
+def CONDITION_ORIGIN_LAUNCH_COUNT_OVER(self,count = 4,all_origins = False):
+    if all_origins:
+        for origin in self.origin:
+            if origin.get_launch_count() < count:
+                return False
+        return True
+    else:
+        for origin in self.origin:
+            if origin.get_launch_count() >= count:
+                return True
+        return False
+
+def CONDITION_ORIGIN_LAUNCH_COUNT_BELOW(self,count = 4,all_origins = False):
+    if all_origins:
+        for origin in self.origin:
+            if origin.get_launch_count() >= count:
+                return False
+        return True
+    else:
+        for origin in self.origin:
+            if origin.get_launch_count() < count:
+                return True
+        return False
 
 
