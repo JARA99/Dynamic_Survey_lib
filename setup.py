868f93ab48898e0f6d55bf180036fae68955672e
Jorge Alejandro <jorgealejandro1999@gmail.com>
2023-07-28 22:35:27 -0600

    Updating setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open('requirements.txt','r') as req:
 
 setup(
     name='pydyn_surv',
-    version='0.1.0',
+    version='1.0.0',
     description='A package with tools for generating a dynamic survey using Machine Learning tools.',
     url='https://github.com/JARA99/Dynamic_Survey_lib',
     author='Jorge Alejandro Rodríguez Aldana',
     author_email='jorgealejandro1999@gmail.com',
     license='Proprietary License',
-    packages=['pydyn_surv','pydyn_surv.LinearClassifier','pydyn_surv.classes'],
+    packages=['pydyn_surv'],
     install_requires=requirements,
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: Other/Proprietary License',  
         'Operating System :: POSIX :: Linux',        
